a1ca1a88279000a94d4424f026d675a7a4b35323
Erik <erikmikaelnyberg@gmail.com>
2024-12-10 13:55:00 +0100

    Add project files.

---
+++ MainApp/Models/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace MainApp.Models;
+
+public class Todo
+{
+    public string Id { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public bool IsCompleted { get; set; }
+}
---
+++ MainApp/Program.cs
@@ -0,0 +1,25 @@
+﻿while (true)
+{
+    Console.WriteLine("--- TODOLIST ---");
+    Console.WriteLine("[1] Add Todo");
+    Console.WriteLine("[2] Show current Todos");
+    Console.WriteLine("[3] Exit program");
+
+    var userChoice = Console.ReadLine();
+
+    switch (userChoice)
+    {
+        case "1":
+            //Add Todo
+            Console.WriteLine("Add Todos");
+            Console.ReadKey();
+            break;
+        case "2":
+            //Show Todos
+            Console.WriteLine("Show Todos");
+            Console.ReadKey(); 
+            break;
+        case "3":
+            return;
+    }
+}
